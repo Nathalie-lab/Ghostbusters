d30f60c549b97eda86640252a4a79aae9824d970
Nathalie <Nikandra191999@yandex.ru>
2025-03-31 19:56:35 +0300

    добавила основные файлы

---
+++ main.js
@@ -0,0 +1,9 @@
+function showHtmlDiv() {
+    var htmlShow = document.getElementById("showhide");
+    if (htmlShow.style.display === "none") {
+        htmlShow.style.display = "block";
+    }
+    else {
+        htmlShow.style.display= "none";
+    }
+}
